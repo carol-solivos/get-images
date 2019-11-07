5de880346d1561467ed9309d63a718ddeea83b52
Carol Sepúlveda <carolsepulveda@Carols-MacBook-Pro.local>
2019-11-07 10:16:40 -0300

    create list file

--- get-files.js
+++ get-files.js
@@ -5,4 +5,14 @@ let dirBuf = Buffer.from(directory);
 
 let files = fs.readdirSync(directory);
 
-console.log(files.filter(x => x.includes('.svg')));
+let list = files.filter(x => x.includes('.svg'));
+console.log(list);
+
+fs.writeFile("array-gen/list.txt", list, function(err) {
+
+	if(err) {
+			return console.log(err);
+	}
+
+	console.log("The file was saved!");
+}); 
